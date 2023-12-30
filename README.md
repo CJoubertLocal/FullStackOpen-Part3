c00d8c633c3d69e84b518b099aa57e2745a10f03
CJoubertLocal <chrinnetwrok@gmail.com>
2023-12-30 12:59:40 +0000

    Exercise 3.10, reorganised code into frontend and backend directories, deployed app to server, and created a README.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FullStackOpen-Part3
+# # # Phonebook 
+
+This is a phonebook application using react on the frontend and node & express on the backend. It allows one to see contacts in a phonebook. Those in the original phonebook have fictional contact details.
+
+Link to the application: https://2023.fly.dev/
---
+++ backend/index.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('dist'))
+
+// based on getMethodToken
+// at https://github.com/expressjs/morgan/blob/master/index.js
+morgan.token('body', function getReqBody(req) {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+let entries = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (request, response) => {
+  const date = new Date();
+
+  info = `<p>
+          Phonebook has info for ${entries.length} people
+        </p>
+        <p>
+          ${date}
+        </p>`
+  response.set('Content-Type', 'text/html');
+  response.send(
+       info
+  )  
+})
+
+app.get('/api/persons', (request, response) => {
+  if (entries) {
+    response.json(entries)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  result = entries.filter(e => e.id === Number(request.params.id))
+
+  if (result.length === 1) {
+    response.send(result)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons/', (request, response)=> {
+  
+  if (request.body.name === undefined) {
+    return response.status(400).json({
+      error: `Please include a name for the person to be added to the phone book.`
+    })
+  }
+  
+  if (request.body.number === undefined) {
+    return response.status(400).json({
+      error: `Please include a number for the person to be added to the phone book.`
+    })
+  }
+
+  if (entries.filter(e => e.name.toLowerCase() === request.body.name.toLowerCase()).length) {
+    return response.status(406).json({
+      error: `There is already a phonebok entry with this exact name.`
+    })
+  }
+
+  // assumes number of entries will never be as large as 100000.
+  // an alternative to take double the current length of the array of entries.
+  const newID = Math.floor(Math.random() * 100000)
+  while (entries.filter(e => e.id === newID).length > 0) {
+    newID = Math.floor(Math.random() * 100000)
+  }
+
+  const newEntry = request.body
+  newEntry.id = newID
+
+  entries = entries.concat(newEntry)
+
+  response.json(newEntry)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  entries = entries.filter(e => e.id !== Number(request.params.id))
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
